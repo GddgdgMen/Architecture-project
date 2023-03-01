0e4d84b31a45bbefe87bed106411d9c1b9ce8119
GddgdgMen <antonsvirskiy690@gmail.com>
2023-03-01 21:03:20 +0200

    Updated main.go

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func getTime(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
-	var Time = time.Now()
+	var currTime = time.Now()
 	var response = make(map[string]string)
-	response["time"] = Time.Format("2006-01-02T15:04:05Z07:00")
+	response["time"] = currTime.Format("2006-01-02T15:04:05Z07:00")
 	var json, err = jsonEnc.Marshal(response)
 
 	if err != nil {
