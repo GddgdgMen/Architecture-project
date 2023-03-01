b0414a9f65e58f16a0a2f54b28bb3d94efbebe9f
Maks <maxim.kryvenok@gmail.com>
2023-03-01 20:11:41 +0200

    Updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	jsonEnc "encoding/json"
+	"log"
 	"net/http"
+	"time"
 )
 
 func getTime(w http.ResponseWriter, r *http.Request) {
@@ -9,4 +12,14 @@ func getTime(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var Time = time.Now()
+	var response = make(map[string]string)
+	response["time"] = Time.Format("2006-01-02T15:04:05Z07:00")
+	var json, err = jsonEnc.Marshal(response)
+
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+
+	w.Write(json)
+
 }
